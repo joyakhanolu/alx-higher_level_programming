1811be3550fd34b518232ade267b151c31f3803d
joy <joyakhanolu17@gmail.com>
2022-05-31 19:13:53 +0000

    Prints a number if positive or negative

---
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10, 10)
+n = float (input ("Enter the number: "))
+if n > 0;
+	print ("The number provided is positive")
+elif n == 0;
+	print ("The number input is zero")
+else:
+	print ("The number provided is negative" '\n')
