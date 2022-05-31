0484b13a27493e6bd078d8dce58a2bf166a576c2
Joy Akhanolu <joyakhanolu17@gmail.com>
2022-05-31 12:24:25 -0700

    print whether the number stored in the variable number is positive or negative

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -2,8 +2,8 @@
 import random
 number = random.randint(-10, 10)
 if number > 0;
-	print ("The number provided is positive")
+	print (number "is positive")
 elif number == 0;
-	print ("The number input is zero")
+	print (number, "is zero")
 else:
-	print ("The number provided is negative")
+	print (number, "is negative")
