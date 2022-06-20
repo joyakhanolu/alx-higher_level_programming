a4b14618a90ff9fc75de4e3cb3164496d3e80090
Joy Akhanolu <joyakhanolu17@gmail.com>
2022-06-20 14:16:38 -0700

    Update 101-safe_function.py

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,12 +1,23 @@
 #!/usr/bin/python3
-
-import sys
-
-
 def safe_function(fct, *args):
+    """
+    Executes a function safely
+
+    Args:
+        fct: pointer to a function
+        args: arguments passed to function
+
+    Return:
+        Result of the function otherwise, None
+    """
+    from sys import stderr
     try:
-        result = fct(*args)
-        return (result)
-    except:
-        print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
-        return (None)
+        return fct(*args)
+    except ZeroDivisionError as err:
+        print('Exception: {}'.format(err), file=stderr)
+    except ValueError as va:
+        print('Exception: {}'.format(va), file=stderr)
+    except TypeError as tp:
+        print('Exception: {}'.format(tp), file=stderr)
+    except IndexError as ind:
+        print('Exception: {}'.format(ind), file=stderr)
